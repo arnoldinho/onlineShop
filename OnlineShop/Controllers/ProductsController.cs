55bd67b6d494a3d0867d83cab1c9448b3ff78e26
Arnold Nowak <arnoldnowak1992@gmail.com>
2016-12-18 02:07:01 +0100

    poprawka2

---
+++ OnlineShop/Controllers/ProductsController.cs
@@ -0,0 +1,126 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Domain.Model;
+using Application;
+//using ProductDB;
+
+namespace OnlineShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //ProductDbContext db = new ProductDbContext();
+        IMethodsRepository db;
+
+        public ProductsController(IMethodsRepository db)
+        {
+            this.db = db;
+        }
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(db.GetProducts().ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.FindById(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NazwaProduktu,cenaProdukty")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.FindById(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NazwaProduktu,cenaProdukty")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Edit(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.FindById(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            Product product = db.FindById(id);
+            db.Remove(product.Id);
+            return RedirectToAction("Index");
+        }
+        /*
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
+    }
+}
